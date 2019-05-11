bba852d2ec916bb052a8e888b6295e75de99cd53
KEINOS <github@keinos.com>
2019-05-12 08:44:23 +0900

    fix: Delay test until host is up
    Pinging 5 times with 3 sec sleep

--- Tests/run-test-container.sh
+++ Tests/run-test-container.sh
@@ -7,16 +7,43 @@ set -eu
 
 cd $(dirname $0)
 
-# Random value
-text_expect=$(cat /dev/urandom | base64 | fold -w 10 | head -n 1)
+# Function to Ping host
+function is_host_up () {
+    result=1
+    ping -c 3 -t 3 -s 32 $NAME_HOST > /dev/null;
+    if [ $? -eq 0 ]; then
+        #success
+        result=0
+    fi
+    return $result
+}
 
-text_result=$(curl "http://${NAME_HOST}:${PORT_HOST_TEST}/app1/?test=${text_expect}") && \
-docker-compose down
+# Check is host up
+echo "Host Name: ${NAME_HOST}"
+echo "Host Port: ${PORT_HOST_TEST}"
+retry_max=5
+while ! is_host_up; do
+    retry=$(expr ${retry:-0} + 1)
+    echo 'Pinging host ...'
+    sleep 3
+    if [ $retry -gt $retry_max ]; then
+      echo '  * Host is down'
+      exit 1
+    fi
+    echo "Retry: ${retry}"
+done
+echo '  - Host is up'
+
+# Get random value to send
+text_expect=$(cat /dev/urandom | base64 | fold -w 10 | head -n 1 | sed -e 's/\+/X/g')
 
 # Test
-echo "Host Port: ${PORT_HOST_TEST}"
+text_result=$(curl "http://${NAME_HOST}:${PORT_HOST_TEST}/app1/?test=${text_expect}")
+
 echo "Expect   : ${text_expect}"
 echo "Result   : ${text_result:-error}"
+
+echo 'Running test ...'
 if [ "${text_expect}" = "${text_result}" ]; then
    echo 'Test passed.'
    exit 0
--- Tests/run-test-locally.sh
+++ Tests/run-test-locally.sh
@@ -2,13 +2,30 @@
 
 # Run this script to check locally.
 
-cd $(dirname $0)
-
 source PORT_HOST_TEST.env
 
 NAME_HOST='localhost'
+NAME_IMAGE='webapi-proxy:test'
+DIR_CURRENT=$(cd $(dirname $0); pwd)
+DIR_PARRENT=$(dirname $(cd $(dirname $0); pwd))
 
-docker-compose up -d && \
+# Build image
+cd $DIR_PARRENT
+docker build --tag $NAME_IMAGE .
+if [ $? -ne 0 ]; then
+  echo 'Failed to build image'
+  exit 1
+fi
+
+# Run tests
+cd $DIR_CURRENT && \
+docker-compose up -d --build && \
 docker container ls | grep app1 && \
 source run-test-container.sh && \
 docker-compose down
+
+# Remove image
+cd $DIR_PARRENT
+docker container prune -f && \
+docker image prune -f && \
+docker image rm NAME_IMAGE
