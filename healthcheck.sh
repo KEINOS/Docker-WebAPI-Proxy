9a7fe5f75e439ca03c19dd8fb5b084e0dac7accc
KEINOS <github@keinos.com>
2019-05-12 11:05:30 +0900

    feat: delay until nginx is up

--- healthcheck.sh
+++ healthcheck.sh
@@ -3,4 +3,13 @@
 # Alpine doesn't have cURL by default
 #curl --fail http://localhost/ || exit 1
 
+function is_host_up () {
+    netstat -tulpn | grep :80 | grep nginx
+    return $?
+}
+
+while ! is_host_up; do
+  sleep 1
+done
+
 wget -q --spider http://localhost/ || exit 1
