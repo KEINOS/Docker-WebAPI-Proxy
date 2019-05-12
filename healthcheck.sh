46839c165e510a4c448d82cd873e4c8d5ec51e02
KEINOS <github@keinos.com>
2019-05-12 11:45:11 +0900

    fix: prevent endless loop

--- healthcheck.sh
+++ healthcheck.sh
@@ -3,13 +3,23 @@
 # Alpine doesn't have cURL by default
 #curl --fail http://localhost/ || exit 1
 
+set -eu
+
 function is_host_up () {
     netstat -tulpn | grep :80 | grep nginx
     return $?
 }
 
+count=1
+count_max=30
 while ! is_host_up; do
+  echo "Waiting to boot Nginx(${count}/${count_max})"
+  if [ $count -gt $count_max ]; then
+    echo 'Can not boot nginx.'
+    exit 1
+  fi
   sleep 1
+  count=$(expr $count + 1)
 done
 
 wget -q --spider http://localhost/ || exit 1
